82cdd8eb50184bb92291f3548f471ae8a2439d9f
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-01-31 12:49:44 +0200

    task-#5-pass-user-details
    - added application.yml to gitignore
    - added new gateway pre-filter to parse the JWT token from the Authorization Bearer header and pass the info via headers to downstream services
    - 2 new dependencies needed for parsing the JWT token

---
+++ src/main/java/com/notabarrista/gateway/filter/SCGPreFilter.java
@@ -0,0 +1,78 @@
+package com.notabarrista.gateway.filter;
+
+import com.okta.jwt.AccessTokenVerifier;
+import com.okta.jwt.Jwt;
+import com.okta.jwt.JwtVerificationException;
+import com.okta.jwt.JwtVerifiers;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+/**
+ * Add '- name: SCGPreFilter' to the gateway routes config for this to be used
+ */
+@Component
+public class SCGPreFilter extends AbstractGatewayFilterFactory<SCGPreFilter.Config> {
+
+    @Value( "${okta.oauth2.issuer}" )
+    private String jwtIssuer;
+
+    @Value( "${okta.oauth2.audience}" )
+    private String jwtAudience;
+
+    public SCGPreFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // there's an alternative to look into the request principal but only for uid
+
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest().mutate().build();
+            //adds header to proxied request
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            if (headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+                String jwtString = headers.getFirst(HttpHeaders.AUTHORIZATION);
+                jwtString = jwtString.replace("Bearer ", "");
+                AccessTokenVerifier jwtVerifier = JwtVerifiers.accessTokenVerifierBuilder()
+                                                              .setIssuer(jwtIssuer)
+                                                              .setAudience(jwtAudience)                   // defaults to 'api://default'
+                                                              .setConnectionTimeout(Duration.ofSeconds(1))    // defaults to 1s
+                                                              .setRetryMaxAttempts(2)                     // defaults to 2
+                                                              .setRetryMaxElapsed(Duration.ofSeconds(10)) // defaults to 10s
+                                                              .build();
+                try {
+                    Jwt jwt = jwtVerifier.decode(jwtString);
+                    request = exchange.getRequest().mutate()
+                                      .header("uid", jwt.getClaims().get("uid").toString())
+                                      .header("emailClaim", jwt.getClaims().get("emailClaim").toString())
+                                      .header("claims", jwt.getClaims().toString())
+                                      .build();
+                } catch (JwtVerificationException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+    public static class Config {
+        private String name;
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
