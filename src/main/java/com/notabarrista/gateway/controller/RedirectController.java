0d143340732fdcad8118659f218a77f7c2626fa8
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-03-09 18:36:38 +0200

    added redirect URL to the authenticate endpoint

--- src/main/java/com/notabarrista/gateway/controller/RedirectController.java
+++ src/main/java/com/notabarrista/gateway/controller/RedirectController.java
@@ -1,5 +1,6 @@
 package com.notabarrista.gateway.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
@@ -7,6 +8,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.RedirectView;
 
 /**
@@ -19,9 +21,15 @@ import org.springframework.web.reactive.result.view.RedirectView;
 public class RedirectController {
 
     @RequestMapping("/authenticate")
-    public RedirectView checkAuth(@AuthenticationPrincipal OidcUser oidcUser, Model model,
-                                 @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client) {
-        return new RedirectView("/");
+    public RedirectView checkAuth(@RequestParam(required = false) String redirectUrl, @AuthenticationPrincipal OidcUser oidcUser, Model model,
+                                  @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client) {
+        if (StringUtils.isBlank(redirectUrl)) {
+            redirectUrl = "/";
+        } else if (!redirectUrl.startsWith("/")) {
+            redirectUrl = "/" + redirectUrl;
+        }
+
+        return new RedirectView(redirectUrl);
     }
 
 }
