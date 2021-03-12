af5237af5b0f65cc0eeefa2a80aa7866f6259440
Tudor Codrea <tudor.codrea@softvision.com>
2021-03-12 23:42:10 +0200

    reverted SCGPreFilter to use Authorization header
    using token to get OKTA uid, email and claims as before
    Principal way misleaded us with regards to uid when calling from browser and calling endpoints with postman filling in email instead of uid

--- src/main/java/com/notabarrista/gateway/OktaOAuth2WebSecurity.java
+++ src/main/java/com/notabarrista/gateway/OktaOAuth2WebSecurity.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.ServerWebExchange;
 
 /**
  * @author codrea.tudor
@@ -16,27 +15,17 @@ import org.springframework.web.server.ServerWebExchange;
 @EnableWebFluxSecurity
 public class OktaOAuth2WebSecurity {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
-    {
-//        http.csrf().disable()
-//                .authorizeExchange()
-//                .anyExchange()
-//                .authenticated()
-//                .and().oauth2Login()
-//                .and().oauth2ResourceServer().jwt();
-        
-    	http.csrf().disable();
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+		http.csrf().disable();
 		http.authorizeExchange().pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.pathMatchers("/*", "/static/**", "/assets/**", "/actuator/**", "/admin/**", "/doc-service/**",
 						"/discovery/**", "/eureka/**", "/health/**", "/loggers/**")
-				.permitAll().anyExchange()
-                .authenticated()
-                .and().oauth2Login()
-                .and().oauth2ResourceServer().jwt();
+				.permitAll().anyExchange().authenticated().and().oauth2Login().and().oauth2ResourceServer().jwt();
 		http.headers().frameOptions().disable();
 		http.headers().xssProtection();
-        
-        return http.build();
-    }
+
+		return http.build();
+	}
 }
--- src/main/java/com/notabarrista/gateway/filter/SCGPreFilter.java
+++ src/main/java/com/notabarrista/gateway/filter/SCGPreFilter.java
@@ -1,10 +1,17 @@
 package com.notabarrista.gateway.filter;
 
+import com.okta.jwt.AccessTokenVerifier;
+import com.okta.jwt.Jwt;
+import com.okta.jwt.JwtVerificationException;
+import com.okta.jwt.JwtVerifiers;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
-import java.security.Principal;
+import java.time.Duration;
 
 /**
  * Add '- name: SCGPreFilter' to the gateway routes config for this to be used
@@ -18,20 +25,47 @@ import java.security.Principal;
 @Component
 public class SCGPreFilter extends AbstractGatewayFilterFactory<SCGPreFilter.Config> {
 
+    @Value( "${okta.oauth2.issuer}" )
+    private String jwtIssuer;
+
+    @Value( "${okta.oauth2.audience}" )
+    private String jwtAudience;
+
     public SCGPreFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> exchange.getPrincipal()
-                                            .map(Principal::getName)
-                                            .map(userId -> {
-                                                //adds header to proxied request
-                                                exchange.getRequest().mutate().header("uid", userId).build();
-                                                return exchange;
-                                            })
-                                            .flatMap(chain::filter);
+
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest().mutate().build();
+            //adds header to proxied request
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            if (headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+                String jwtString = headers.getFirst(HttpHeaders.AUTHORIZATION);
+                jwtString = jwtString.replace("Bearer ", "");
+                AccessTokenVerifier jwtVerifier = JwtVerifiers.accessTokenVerifierBuilder()
+                                                              .setIssuer(jwtIssuer)
+                                                              .setAudience(jwtAudience)                   // defaults to 'api://default'
+                                                              .setConnectionTimeout(Duration.ofSeconds(1))    // defaults to 1s
+                                                              .setRetryMaxAttempts(2)                     // defaults to 2
+                                                              .setRetryMaxElapsed(Duration.ofSeconds(10)) // defaults to 10s
+                                                              .build();
+                try {
+                    Jwt jwt = jwtVerifier.decode(jwtString);
+                    request = exchange.getRequest().mutate()
+                                      .header("uid", jwt.getClaims().get("uid").toString())
+                                      .header("email", jwt.getClaims().get("email").toString())
+                                      .header("claims", jwt.getClaims().toString())
+                                      .build();
+                } catch (JwtVerificationException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return chain.filter(exchange.mutate().request(request).build());
+        };
     }
 
     public static class Config {
